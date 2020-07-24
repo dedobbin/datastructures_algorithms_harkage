308fb0ac216e85351f42b145d621f815ad733bb0
debobbin <you@example.com>
2020-07-24 23:06:14 +0200

    Added clean to make files

--- data_structures/main.cpp
+++ data_structures/main.cpp
@@ -7,5 +7,14 @@ int main (int argv, char* argc[])
 	list->insert(2);
 	list->insert(3);
 	std::cout << *list << std::endl;
+
+	auto list2 = new LinkedList<std::string>();
+	list2->insert("one");
+	list2->insert("two");
+	list2->insert("three");
+	std::cout << *list2 << std::endl;
+
+
 	delete(list);
+	delete(list2);
 }
